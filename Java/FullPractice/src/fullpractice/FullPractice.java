d7ae6e411abc64b43382162b888c8749d6ce36d8
alwayzmile <alwayz.for.zmile@gmail.com>
2015-12-19 03:23:24 +0700

    ADD NetBeans project for simple practices
    This FullPractice NetBeans project will contains a lot of java classes
    which may not related one another. Just collection of my Java
    programming practice.

---
+++ Java/FullPractice/src/fullpractice/FullPractice.java
@@ -0,0 +1,10 @@
+package fullpractice;
+
+public class FullPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Doing nothing. "
+                + "SHIFT + F6 on java file to run each file in FullPractice (NetBeans)");
+    }
+    
+}
