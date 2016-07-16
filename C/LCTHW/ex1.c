c0e13b34a2a589f65b48598b0d508cebbfef6b91
alwayzmile <alwayz.for.zmile@gmail.com>
2016-07-16 20:12:36 +0700

    Exercise with Learn C the Hard Way (1-14)

---
+++ C/LCTHW/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("Hello world2.");
+  puts("Hello world3.");
+  puts("Hello world4.");
+  puts("Hello world5.");
+  puts("Hello world6.");
+
+  return 0;
+}
